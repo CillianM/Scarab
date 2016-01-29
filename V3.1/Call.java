8330772587c5a5947eac22d4351295fc17d4dd2b
Cillian Mc Neill <cillianmcneill@gmail.com>
2016-01-29 00:32:40 +0000

    Change around
    Moved files around, command line version visible, only current version
    of gui visible

--- V3.1/Call.java
+++
@@ -1,8 +0,0 @@
-public class  Call
-{	
-	public static void main(String [] args)
-	{
-		Scarab scarab = new Scarab();
-		scarab.setVisible(true);
-	}
-}
--- V3/Call.java
+++
@@ -1,9 +0,0 @@
-//simple call to get the programme going
-public class  Call
-{	
-	public static void main(String [] args)
-	{
-		Scarab scarab = new Scarab();
-		scarab.setVisible(true);
-	}
-}
